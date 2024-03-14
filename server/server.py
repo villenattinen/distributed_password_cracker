d4238947a756497bc888b3a997c18294c86f65be
Ville Nättinen <villenattinen@gmail.com>
2024-03-14 16:41:17 +0200

    Initialize logging

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 import socket
+import logging
 
 # handle work request
 # hashcat/johntheripper
@@ -6,7 +7,6 @@ import socket
 # default wordlist/dictionary
 # communicate with worker
 # send response back to client
-# 
 
 class Server:
     def __init__(self, address, port):
@@ -15,21 +15,27 @@ class Server:
         self.serverSocket.bind(self.serverAddress)
 
     def handle_request(self, data, address):
-        print('Received message:', data.decode(), 'from', address)
-        response = 'Received message: ' + data.decode() 
-        self.serverSocket.sendto(response.encode(), address)
+        print('Received:', data.decode(), 'from', address)
+        logging.info('Received: %s from %s', data.decode(), address)
+        self.handle_response(data, address)
 
     def handle_response(self, data, address):
-        print('Received response:', data.decode(), 'from', address)
-        response = 'Received response: ' + data.decode() 
-        self.serverSocket.sendto(response.encode(), address)
+        logging.info('Sending response: %s to %s', data.decode(), address)
+        self.serverSocket.sendto(data, address)
 
     def run(self):
+        logging.info('Server running')
         while True:
             print('Waiting for a message...')
             data, address = self.serverSocket.recvfrom(1024)
             self.handle_request(data, address)
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        filename='server.log',
+        level=logging.INFO,
+        format='%(asctime)s:%(levelname)s:%(message)s'
+    )
+    logging.info('Launching server...')
     server = Server('localhost', 9090)
     server.run()
