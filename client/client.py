4b22ebc2cef96ef1fbc6d00914b9cf8e979123b5
Ville Nättinen <villenattinen@gmail.com>
2024-03-14 19:37:23 +0200

    Add comments

--- client/client.py
+++ client/client.py
@@ -2,40 +2,28 @@ import socket
 # import time
 # import sys
 
-#   $ hashcat -m value -a value hashfile wordlist
-#   excluding combination attacks, mask attacks
-
-
-#   $ john --format=value hashfile
-#   excluding zip-file, unshadow, etc.
-#   
-#   crack hashstring [] or crack -h/--help 
-#   --help/-h               display the options
-#   --hashcat/-cat          only crack using hashcat
-#   --johntheripper/-john   only crack using johntheripper
-#   --bruteforce/-bf        only crack using bruteforce
-#   --wordlist/-wl          only crack using wordlist
-#   --hashtype/-ht          specify the hash type manually
-#   --exit/e                exit the program
-
 class Client:
+    # Initialize the client class
     def __init__(self, address, port):
         self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.serverAddress = (address, port)
 
+    # Send a message
     def send(self, message):
         self.clientSocket.sendto(message.encode(), self.serverAddress)
     
+    # Receive a message
     def receive(self):
         data, server = self.clientSocket.recvfrom(1024)
         return data.decode()
     
+    # Close the connection
     def close(self):
         self.clientSocket.close()
 
 if __name__ == '__main__':
-    
+    # Start the client
     client = Client('localhost', 9090)
-    client.send('Hello, server!')
+    client.send('PING')
     print(client.receive())
     client.close()
--- server/server.py
+++ server/server.py
@@ -1,28 +1,28 @@
 import socket
 import logging
-
-# handle work request
-# hashcat/johntheripper
-# bruteforce/wordlist
-# default wordlist/dictionary
-# communicate with worker
-# send response back to client
+import time
 
 class Server:
+    # Initialize the server class
     def __init__(self, address, port):
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.serverAddress = (address, port)
         self.serverSocket.bind(self.serverAddress)
 
+    # Handle incoming requests
     def handle_request(self, data, address):
         print('Received:', data.decode(), 'from', address)
         logging.info('Received: %s from %s', data.decode(), address)
+        if data.decode() == 'PING':
+            self.handle_response('PONG'.encode(), address)
         self.handle_response(data, address)
 
+    # Send a response
     def handle_response(self, data, address):
         logging.info('Sending response: %s to %s', data.decode(), address)
         self.serverSocket.sendto(data, address)
 
+    # Run the server
     def run(self):
         logging.info('Server running')
         while True:
@@ -31,11 +31,14 @@ class Server:
             self.handle_request(data, address)
 
 if __name__ == '__main__':
+    # Set up logging
     logging.basicConfig(
         filename='server.log',
         level=logging.INFO,
         format='%(asctime)s:%(levelname)s:%(message)s'
     )
     logging.info('Launching server...')
+
+    # Start the server
     server = Server('localhost', 9090)
     server.run()
